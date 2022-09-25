7321fefc4c56ebafa47b6e8e607b92c45f0d684f
Noe Benjamin Reynoso <nreynoso@geniuscode.mx>
2022-09-25 10:33:02 -0700

    Commit #2 +Se agrego una peticion de tipo json array +misma estructura aplicada para este tipo de peticion

--- app/src/main/java/com/reynoso/parsedata_volley/MainActivity.java
+++ app/src/main/java/com/reynoso/parsedata_volley/MainActivity.java
@@ -2,6 +2,7 @@ package com.reynoso.parsedata_volley;
 
 import static android.content.ContentValues.TAG;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -11,22 +12,46 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 
 public class MainActivity extends AppCompatActivity {
     private RequestQueue queue;
-    private  String url ="https://wwww.google.com";
+    private String url ="https://google.com";
+    private  String API ="https://jsonplaceholder.typicode.com/users/";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        googleStringRequest();
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, API, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                Log.d(TAG, "onResponse() returned: " + response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "onErrorResponse: ",error );
+            }
+        });
+        queue.add(googleStringRequest());
+        queue.add(jsonArrayRequest);
+    }
+
+    @NonNull
+    private StringRequest googleStringRequest() {
         queue = Volley.newRequestQueue(MainActivity.this);
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
-                Log.d(TAG, "onResponse: "+response);
+                Log.d(TAG, "onResponse: "+response.substring(0,500));
             }
         }, new Response.ErrorListener() {
             @Override
@@ -34,6 +59,6 @@ public class MainActivity extends AppCompatActivity {
                 Log.e(TAG, "onErrorResponse: ", error.fillInStackTrace() );
             }
         });
-    queue.add(stringRequest);
+        return  stringRequest;
     }
 }
